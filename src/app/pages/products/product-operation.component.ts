82345c341e624a316bc3fc0e5402391c6f162e38
Panha <76092165+Panha13@users.noreply.github.com>
2024-08-10 11:40:14 +0700

    update html

--- src/app/pages/products/product-operation.component.ts
+++ src/app/pages/products/product-operation.component.ts
@@ -13,7 +13,180 @@ import { Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-product-operation',
-  templateUrl: './product-operation.component.html',
+  template: `
+    <div nz-row nzJustify="center">
+      <form
+        class="form-container"
+        nz-form
+        [nzLayout]="'vertical'"
+        [formGroup]="form"
+      >
+        <div nz-row nzGutter="16">
+          <div nz-col [nzSpan]="12">
+            <nz-form-item>
+              <nz-form-label class="required-marker"
+                >Product Name</nz-form-label
+              >
+              <nz-form-control
+                [nzErrorTip]="
+                  form.get('name')?.hasError('required')
+                    ? 'Please enter product name'
+                    : ''
+                "
+              >
+                <input
+                  nz-input
+                  placeholder="Enter product name"
+                  formControlName="name"
+                  type="text"
+                  autocomplete="true"
+                />
+              </nz-form-control>
+            </nz-form-item>
+            <nz-form-item>
+              <nz-form-label class="required-marker">Price</nz-form-label>
+              <nz-form-control
+                [nzErrorTip]="
+                  form.get('price')?.hasError('required')
+                    ? 'Please enter price'
+                    : ''
+                "
+              >
+                <nz-input-number
+                  [nzMin]="0"
+                  [nzStep]="0.1"
+                  formControlName="price"
+                  [nzPlaceHolder]="'Please enter price'"
+                  class="w-100"
+                ></nz-input-number>
+              </nz-form-control>
+            </nz-form-item>
+          </div>
+          <div nz-col [nzSpan]="12">
+            <nz-form-item>
+              <nz-upload
+                [nzAction]="uploadUrl"
+                [(nzFileList)]="fileProfile"
+                [nzBeforeUpload]="beforeUpload"
+                (nzChange)="handleImageUpload($event)"
+                nzListType="picture-card"
+                [nzShowButton]="fileProfile.length < 1"
+              >
+                <div>
+                  <i nz-icon nzType="plus"></i>
+                </div>
+              </nz-upload>
+            </nz-form-item>
+          </div>
+        </div>
+
+        <div nz-row nzGutter="16">
+          <div nz-col [nzSpan]="12">
+            <nz-form-item>
+              <nz-form-label class="required-marker">Stock</nz-form-label>
+              <nz-form-control
+                [nzErrorTip]="
+                  form.get('stock_quantity')?.hasError('required')
+                    ? 'Please enter stock quantity'
+                    : ''
+                "
+              >
+                <nz-input-number
+                  [nzMin]="0"
+                  [nzStep]="1"
+                  [nzPrecision]="0"
+                  formControlName="stock_quantity"
+                  [nzPlaceHolder]="'Please enter quantity'"
+                  class="w-100"
+                ></nz-input-number>
+              </nz-form-control>
+            </nz-form-item>
+          </div>
+          <div nz-col [nzSpan]="12">
+            <nz-form-item>
+              <nz-form-label class="required-marker">Unit</nz-form-label>
+              <nz-form-control
+                [nzErrorTip]="
+                  form.get('unit_id')?.hasError('required')
+                    ? 'Please enter product unit'
+                    : ''
+                "
+              >
+                <nz-select
+                  nzShowSearch
+                  nzAllowClear
+                  nzPlaceHolder="Select Unit"
+                  formControlName="unit_id"
+                >
+                  <nz-option
+                    *ngFor="let unit of units"
+                    [nzLabel]="unit.name"
+                    [nzValue]="unit.unit_id"
+                  ></nz-option>
+                </nz-select>
+              </nz-form-control>
+            </nz-form-item>
+          </div>
+        </div>
+        <nz-form-item>
+          <nz-form-label>Category</nz-form-label>
+          <nz-form-control>
+            <nz-select
+              nzShowSearch
+              nzAllowClear
+              nzPlaceHolder="Select category"
+              formControlName="category_id"
+            >
+              <nz-option
+                *ngFor="let category of categories"
+                [nzLabel]="category.name"
+                [nzValue]="category.category_id"
+              ></nz-option>
+            </nz-select>
+          </nz-form-control>
+        </nz-form-item>
+        <nz-form-item>
+          <nz-form-label>Description</nz-form-label>
+          <nz-form-control
+            [nzErrorTip]="
+              form.get('description')?.hasError('required')
+                ? 'Please enter description'
+                : ''
+            "
+          >
+            <textarea
+              nz-input
+              placeholder="Product description"
+              [nzAutosize]="{ minRows: 3, maxRows: 3 }"
+              formControlName="description"
+            ></textarea>
+          </nz-form-control>
+        </nz-form-item>
+        <nz-form-item>
+          <nz-form-control style="text-align: end;">
+            <button
+              nz-button
+              nzType="default"
+              type="button"
+              style="margin-right: 10px;"
+              (click)="onCancel()"
+            >
+              Cancel
+            </button>
+            <button
+              nz-button
+              nzType="primary"
+              [disabled]="!form.valid || loading"
+              (click)="onSubmit()"
+            >
+              <i *ngIf="loading" nz-icon nzType="loading"></i>
+              Submit
+            </button>
+          </nz-form-control>
+        </nz-form-item>
+      </form>
+    </div>
+  `,
   styles: [
     `
       .w-100 {
