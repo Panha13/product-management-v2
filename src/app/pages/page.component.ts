c0f046573f1894ea5e5feaa986b2857edee8130e
Panha <76092165+Panha13@users.noreply.github.com>
2024-08-20 15:10:04 +0700

    prepare folder

---
+++ src/app/pages/page.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page',
+  template: `
+   <p>
+  		page Works!
+   </p>
+  `,
+  styles: []
+})
+export class PageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
